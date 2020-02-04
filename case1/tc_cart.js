a1456e725c80045492eee50bc044c35e5d27c1e7
Jimmy <jtorres244@west-mec.org>
2020-02-03 18:25:51 -0700

    cart Html and Cart Javascript complete. Order Javascript in progress.

---
+++ case1/tc_cart.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Jimmy Torres 
+   Date: 2/3/20   
+   
+   Filename: tc_cart.js
+	
+*/
+
+
+
+var orderTotal = 0;
+
+var cartHTML = "<table> <tr> <th>Item</th> <th>Description</th> <th>Price</th> <th>Qty</th> <th>Total</th> </tr>"
+
+for(var i = 0; i < item.length; i++){
+   cartHTML += "<tr> <td><img src='tc_item.png'alt='item'/> </td>"
+}
+
+
+
+
+
+
+
+
+
+
+
--- case1/tc_order.js
+++ case1/tc_order.js
@@ -5,8 +5,8 @@
    Tutorial 10
    Case Problem 1
   
-   Author: 
-   Date:   
+   Author: Jimmy Torres 
+   Date: 2/3/20   
      
    Filename: tc_order.js  
 
@@ -16,4 +16,14 @@
    The itemQty array contains the quantity ordered of each item
    
 */
+
+var item = [10582, 23015, 41807, 10041];
+
+var itemPrice = ["1975 Green Bay Packers Football (signed), item 10582", "Tom Laundry 1955 Football Card (unsigned), item 23015", "1916 Army-Navy Game, Framed Photo (signed), item 41807", "Protective Card Sheets, item 10041"];
+
+var itemQty = [1, 1, 1, 4];
+
+
+
+
 
